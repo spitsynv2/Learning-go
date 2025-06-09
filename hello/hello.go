dbaa259b1bc8bef6ae263d1b1b139b42f2ced3c6
vspitsyn <vadym.spitsyn@gmail.com>
2025-06-09 14:11:30 +0200

    more tests added

--- hello/hello.go
+++ hello/hello.go
@@ -5,14 +5,31 @@ import (
 )
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
-	if name == "" {
-		name = "World"
+func Hello(name string, lang string) string {
+	prefix := ""
+
+	switch lang {
+	case "eng":
+		prefix = englishHelloPrefix
+		if name == "" {
+			name = "World"
+		}
+	case "esp":
+		prefix = spanishHelloPrefix
+		if name == "" {
+			name = "Mundo"
+		}
+	default:
+		prefix = englishHelloPrefix
+		if name == "" {
+			name = "World"
+		}
 	}
-	return englishHelloPrefix + name
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "eng"))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -5,23 +5,39 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	t.Run("hello to people", func(t *testing.T) {
-		got := Hello("people")
+	t.Run("hello to people, empty language", func(t *testing.T) {
+		got := Hello("people", "")
 		expected := "Hello, people"
 
 		assertCorrectMessage(t, got, expected)
 
 	})
+	t.Run("hello to people, eng", func(t *testing.T) {
+		got := Hello("people", "eng")
+		expected := "Hello, people"
+
+		assertCorrectMessage(t, got, expected)
+	})
 	t.Run("hello to world", func(t *testing.T) {
-		got := Hello("world")
-		expected := "Hello, world"
+		got := Hello("World", "")
+		expected := "Hello, World"
 
 		assertCorrectMessage(t, got, expected)
 
 	})
 	t.Run("with empty string", func(t *testing.T) {
-		got := Hello("world")
-		expected := "Hello, world"
+		got := Hello("", "")
+		expected := "Hello, World"
+		assertCorrectMessage(t, got, expected)
+	})
+	t.Run("with empty string, eng", func(t *testing.T) {
+		got := Hello("", "eng")
+		expected := "Hello, World"
+		assertCorrectMessage(t, got, expected)
+	})
+	t.Run("with empty string, spanish", func(t *testing.T) {
+		got := Hello("", "esp")
+		expected := "Hola, Mundo"
 		assertCorrectMessage(t, got, expected)
 	})
 }
