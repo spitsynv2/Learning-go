9520fff706bc25334788c065a674871630e401ce
vspitsyn <vadym.spitsyn@gmail.com>
2025-06-13 12:00:10 +0200

    mock with SpySleeper (not ordered)

---
+++ learnGoWithTests/mocking/countdown_test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestCountdown(t *testing.T) {
+	t.Run("print 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		spySleeper := &SpySleeper{}
+
+		Countdown(buffer, spySleeper)
+
+		got := buffer.String()
+		expected := "3\n2\n1\nGo!"
+
+		if got != expected {
+			t.Errorf("got %q want %q", got, expected)
+		}
+
+		if spySleeper.Calls != 3 {
+			t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
+		}
+	})
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
+}
