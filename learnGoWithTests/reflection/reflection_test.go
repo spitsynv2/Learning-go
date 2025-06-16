6dc33bfd63c088b7f04d64ba7d923f199381f8ce
vspitsyn <vadym.spitsyn@gmail.com>
2025-06-16 11:41:49 +0200

    unsafe nested fields, some nested fields declaretion issue

--- learnGoWithTests/reflection/reflection_test.go
+++ learnGoWithTests/reflection/reflection_test.go
@@ -5,6 +5,16 @@ import (
 	"testing"
 )
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
 func TestWalk(t *testing.T) {
 
 	cases := []struct {
@@ -25,6 +35,38 @@ func TestWalk(t *testing.T) {
 				Name string
 				City string
 			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		{
+			"struct with non string field",
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		/*
+			{
+				"nested fields",
+				struct {
+					Name    string
+					Profile struct {
+						Age  int
+						City string
+					}
+				}{"Chris", struct {
+					Age  int
+					City string
+				}{33, "London"}},
+				[]string{"Chris", "London"},
+			},
+		*/
+		{
+			"nested fields",
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
 			[]string{"Chris", "London"},
 		},
 	}
